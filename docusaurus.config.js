c2133979562c79ffa680059d82b31bd6a326619c
prem315 <prem.patel3105@gmail.com>
2023-10-23 20:16:26 +0530

    configure

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,7 +21,7 @@ const config = {
   organizationName: "prem315", // Usually your GitHub org/user name.
   projectName: "quash-docs", // Usually your repo name.
 
-  onBrokenLinks: "throw",
+  onBrokenLinks: "ignore",
   onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
